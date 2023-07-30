26ab86972e4ddb0520ba39bf2a5f2a526a3e273e
luciaskim <97372163+luciaskimn@users.noreply.github.com>
2023-07-30 18:06:46 +1200

    Install remark-mark and rehype-katex to render LaTeX

--- pages/[category]/[id].js
+++ pages/[category]/[id].js
@@ -4,6 +4,7 @@ import { getMDXComponent } from "mdx-bundler/client";
 import { getAllPostPaths, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
 
+
 export async function getStaticPaths() {
   const paths = getAllPostPaths();
   return {
@@ -25,10 +26,10 @@ export const getStaticProps = async ({ params }) => {
 
 export default function Post({ code, frontmatter }) {
     const Component = useMemo(() => getMDXComponent(code), [code]); 
-    // Memoises components to improve speed, custom name Component
-    
+
     return (
         <article>
+            {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css" integrity="sha384-RZU/ijkSsFbcmivfdRBQDtwuwVqK7GMOw6IMvKyeWL2K5UAlyp6WonmB8m7Jd0Hn" crossorigin="anonymous"></link> */}
             <header>
                 <h1>{frontmatter.title}</h1>
                 <p>{frontmatter.category.charAt(0).toUpperCase() + frontmatter.category.substr(1)} | <Date dateString={frontmatter.publishedAt}/></p>
